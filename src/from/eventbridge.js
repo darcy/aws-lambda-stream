09645efc3b601a81c9678d44c11ec62f12731874
Darcy Brown <darcy@darcybrown.com>
2021-05-13 09:20:10 -0500

    feat: support detail objects in incoming event bridge events

--- src/from/eventbridge.js
+++ src/from/eventbridge.js
@@ -9,13 +9,18 @@ export const fromEventBridge = (event) =>
     // create a stream to work with the rest of the framework
     record: event,
   }])
-    .map(faulty((uow) => ({
+    .map(faulty((uow) => {
+      const detail = (typeof uow.record.detail === 'string')
+        ? JSON.parse(uow.record.detail)
+        : uow.record.detail;
+      return {
       ...uow,
       event: {
-        id: uow.record.detail.id || uow.record.id,
-        ...JSON.parse(uow.record.detail),
+        id: detail.id || uow.record.id,
+        ...detail
       },
-    })));
+    }
+    }));
 
 // test helper
 export const toEventBridgeRecord = (event) => ({
